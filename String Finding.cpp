6c52e99de23ab51c6f042a6ab9a992a71e71853b
AMAN KUMAR VERMA <96662657+Aman2913@users.noreply.github.com>
2023-09-29 20:34:28 +0530

    Create String Finding.cpp

---
+++ String Finding.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    string s1;
+    cin>>s1;
+    if((s.find(s1)!=string::npos))
+    {
+        cout<<1<<endl;
+    }
+    else{
+        cout<<0<<endl;
+    }
+    
+}
