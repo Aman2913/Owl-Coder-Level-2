5f4e1ce1da8d5cf779251d4ddfe412ad28716fb5
AMAN KUMAR VERMA <96662657+Aman2913@users.noreply.github.com>
2023-10-02 16:07:35 +0530

    Create Codemind Parentheses.cpp

---
+++ Codemind Parentheses.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+string removeOuterParentheses(string S) {
+        string res;
+        int opened = 0;
+        for (char c : S) {
+            if (c == '(' && opened++ > 0) res += c;
+            if (c == ')' && opened-- > 1) res += c;
+        }
+        return res;
+}
+int main()
+{
+    string s;
+    getline(cin,s);
+    cout<<removeOuterParentheses(s)<<endl;
+}
