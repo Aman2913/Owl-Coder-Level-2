6a0684a367bbcf644b1958f251495b879dc5ebbc
AMAN KUMAR VERMA <96662657+Aman2913@users.noreply.github.com>
2023-10-02 15:46:09 +0530

    Create Count Sorted Vowel Strings.cpp

---
+++ Count Sorted Vowel Strings.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int countVowelStrings(int n) {
+        int f[5] = {1, 1, 1, 1, 1};
+        for (int i = 0; i < n - 1; ++i) {
+            int s = 0;
+            for (int j = 0; j < 5; ++j) {
+                s += f[j];
+                f[j] = s;
+            }
+        }
+        return accumulate(f, f + 5, 0);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<countVowelStrings(n)<<endl;
+}
