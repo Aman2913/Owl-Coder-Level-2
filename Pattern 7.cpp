32ed81f5fc78f824410cbc3bdfe87fcc5ac2155d
AMAN KUMAR VERMA <96662657+Aman2913@users.noreply.github.com>
2023-10-02 14:21:35 +0530

    Create Pattern 7.cpp

---
+++ Pattern 7.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for(char i = 'A' ; i< 'A'+n ;i++)
+    {
+        for(char j = 'A' ; j<=i;j++)
+        {
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
